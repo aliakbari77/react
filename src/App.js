263df316651b0aed596e35aa4291b2ebd35f61ac
Ali Akbari <7731.aa@gmail.com>
2022-04-03 21:13:20 +0430

    feat: create form component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./components/navBar";
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Register from "./components/register";
+import Login from "./components/login";
+import Movie from "./components/movie";
+import { Redirect } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="container p-3">
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/movie" exact component={Movie} />
+            <Redirect from="/" to="/movie" />
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
